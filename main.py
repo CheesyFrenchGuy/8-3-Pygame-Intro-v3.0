4433cfc02942ed1e567b859ce09b2b2aed79759d
CheesyFrenchGuy <thatcheesyfrenchguy@gmail.com>
2020-08-03 18:53:39 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,34 @@
+# 1
+# name = input("What is your name?: \n")
+# print("Hello " + name)
+
+# 2
+# x = 10 
+# print(x)
+
+#3
+# personNumber = int(input("Choose any whole number: \n"))
+
+# if personNumber > 10:
+#   print("Your number is greater than 10")
+# elif personNumber < 10:
+#   print("Your number is less than 10")
+# else:
+#   print("Your number is 10")
+
+#4
+# personNumber2 = int(input("Choose any whole number: \n"))
+# if personNumber2 % 2 == 0:
+#   print("Even")
+# else:
+#   print("Odd")
+
+#5
+# list = ["France", "Belgium", "U.S.A", "China", "Russia"]
+# for i in list:
+#   print(i)
+
+#6
+import random
+
+list2 = []
